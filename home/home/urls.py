0f7db11e4030effe3d6e027cb542445c785d0911
jihoon <jihoon6372@hanmail.net>
2018-04-25 00:06:46 +0900

    game 소스 수정

--- home/home/urls.py
+++ home/home/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from .views import snow
-from django.views.generic import TemplateView
-
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('game/snow/', TemplateView.as_view(template_name="snow.html")),
-    path('game/bullet/', TemplateView.as_view(template_name="bullet.html")),
+    path('game/snow/', SnowView.as_view()),
+    path('game/bullet/', BulletView.as_view()),
 ]
 
 
--- home/home/views.py
+++ home/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def snow(request):
-    return HttpResponse('test_ok')
+# Create your views here.
+class SnowView(TemplateView):
+	template_name = "snow.html"
+
+class BulletView(TemplateView):
+	template_name = 'bullet.html'
